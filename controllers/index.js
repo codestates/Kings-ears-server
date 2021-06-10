014d5c2415aae7e053bf5559eb47a348a37fc048
Hwangsooneun <catalyst8849@gmail.com>
2021-06-10 13:24:01 +0900

    [modify] : / #5 / index.js / set options and route

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    signup: require('./sign/signup'),
+    signout: require('./sign/signout'),
+    signin: require('./sign/signin'),
+    byebye: require('./sign/byebye'),
+    userinfo: require('./sign/userinfo'),
+    getsecret: require('./secret/getsecret'),
+    newsecret: require('./secret/newsecret'),
+    changepw: require('./sign/changepw'),
+  };
+  
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(express.json());
 const port = 80;
+
+const controllers = require("./controllers");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["http://localhost"], // route53이후 수정
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+app.use(cookieParser());
+app.post('/signup', controllers.signup);
+app.post('/signin', controllers.signin);
+app.get('/signout', controllers.signout);
+app.delete('/byebye', controllers.byebye);
+app.get('/user', controllers.userinfo);
+app.get('/list', controllers.getsecret);
+app.post('/new', controllers.newsecret);
+app.patch('/changepw', controllers.changepw)
 
 app.get('/', (req, res) => {
   res.status(201).send('Hello World');
